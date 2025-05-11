44374587bfe56ceb5116ce039cabb927cdfa0b34
Sandeep <sandeepk6858@gmail.com>
2025-05-11 13:44:45 +0530

    setup the error handling

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import express from "express";
+import morgan from "morgan";
+
 const app = express();
+
+// global middlewares
+if(process.env.NODE_ENV === "development"){
+    app.use(morgan("dev"))
+}
 
 app.get("/", (req, res)=>{
     res.status(200).send("Hello from the server side!!!")
